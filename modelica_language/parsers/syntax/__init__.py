40d1722aa743fe4826fbfd91b66af10e781f40b1
ijknabla <ijknabla@gmail.com>
2019-09-03 20:25:32 +0900

    Add syntax rules
    Modelica Specification(3.4) section B.2.2 ~ B.2.7

--- modelica_language/parsers/syntax/__init__.py
+++ modelica_language/parsers/syntax/__init__.py
@@ -1,3 +1,9 @@
 
 from ._keyword import *
 from ._lexical import *
+from ._class_definition import *
+from ._extends import *
+from ._component_clause import *
+from ._modification import *
+from ._equations import *
+from ._expressions import *
