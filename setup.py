9df6128e6c376a037721057a940b7a5aee57388c
ijknabla <ijknabla@gmail.com>
2019-09-03 12:47:54 +0900

    Add empty package modelica_language.parsers.syntax

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ModelicaLanguage",
+    pack="Modelica language for Python3.x",
+    packages=find_packages(),
+    install_requires=[],
+)
