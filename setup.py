17999d3928ee152f276eff36972c44d52c265fc7
ijknabla <ijknabla@gmail.com>
2019-09-04 13:46:35 +0900

    version 0.0.0a1
    complete Modelica Language parser by arpeggio

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ModelicaLanguage",
+    version="0.0.0a1",
     pack="Modelica language for Python3.x",
     packages=find_packages(),
     install_requires=["Arpeggio"],
