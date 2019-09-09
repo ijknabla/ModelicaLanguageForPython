6ff7a4ea1d898f3cef5a0c01d409859e34bc88b2
ijknabla <ijknabla@gmail.com>
2019-09-09 17:13:11 +0900

    Add NDArrayWrapper

--- modelica_language/types/array.py
+++ modelica_language/types/array.py
@@ -1,10 +1,68 @@
 
 __all__ = (
+    "NDArrayWrapper",
+    "ArraySize",
     "array_sizes_from_indices",
 )
 
 import abc
 import collections.abc
+import functools
+import numpy
+
+
+# define NDArrayWrapper
+
+class InheritableNDArray(numpy.ndarray):
+    def __new__(cls, buffer, dtype=None):
+        arr = numpy.array(buffer, dtype=dtype)
+
+        return super(InheritableNDArray, cls).__new__(
+            cls, arr.shape, buffer=arr, dtype=arr.dtype
+        )
+
+
+class StrAsObjectNDArray(InheritableNDArray):
+    def __new__(cls, buffer, dtype=None):
+        if isinstance(dtype, type) and issubclass(dtype, str):
+            dtype = object
+
+        return super(StrAsObjectNDArray, cls).__new__(
+            cls, buffer=buffer, dtype=dtype
+        )
+
+
+class AutoCastNDArray(StrAsObjectNDArray):
+    def __new__(cls, buffer, dtype=None):
+        self = super(AutoCastNDArray, cls).__new__(
+            cls, buffer, dtype
+        )
+        if dtype is not None and self.dtype == object:
+            isinstance_mask = self.isinstance(dtype)
+            if not isinstance_mask.all():
+                self[~isinstance_mask] = list(
+                    map(dtype, self[~isinstance_mask])
+                )
+        return self
+
+    def isinstance(self, class_or_tuple):
+        @functools.partial(numpy.vectorize, otypes=(bool,))
+        def vectorized_isinstance(elem):
+            return isinstance(elem, class_or_tuple)
+        return vectorized_isinstance(self)
+
+
+class NDArrayWrapper(AutoCastNDArray):
+    def __format__(self, format_spec):
+        return "{{{}}}".format(
+            ", ".join(
+                f"{{:{format_spec}}}".format(subarray)
+                for subarray in self
+            )
+        )
+
+
+# define ArraySize, array_sizes_from_indices()
 
 class ArraySize(collections.abc.Hashable):
     @abc.abstractclassmethod
@@ -27,6 +85,12 @@ class FlexibleArraySize(ArraySize):
     
     def __str__(self):
         return ":"
+    
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return True
+        else:
+            return super().__eq__(other)
 
 
 _FlexibleArraySize__instance = object.__new__(FlexibleArraySize)
