62a03ec5fa1186cf2fea2b70a8a7bd988b57337f
ijknabla <ijknabla@gmail.com>
2019-09-10 09:35:46 +0900

    Add Numpy to install_requires

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(
     version="0.0.0a1",
     pack="Modelica language for Python3.x",
     packages=find_packages(),
-    install_requires=["Arpeggio"],
+    install_requires=[
+        "Arpeggio",
+        "Numpy",
+    ],
 )
