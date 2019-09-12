4a77b32519488670a9374a5405ea31d15baa10f1
ijknabla <ijknabla@gmail.com>
2019-09-12 14:34:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
+#!/usr/bin/env python3
 
 from setuptools import setup, find_packages
 
+from modelica_language import __version__ as package_version
+
 setup(
     name="ModelicaLanguage",
-    version="0.0.0a1",
-    pack="Modelica language for Python3.x",
+    version=package_version,
+    description="Modelica parser and class representation for Python3.x",
+    author="ijknabla",
+    author_email="ijknabla@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    url="https://github.com/ijknabla/ModelicaLanguageForPython",
     packages=find_packages(),
     install_requires=[
         "Arpeggio",
         "Numpy",
     ],
+    python_requires='>=3.6',
 )
