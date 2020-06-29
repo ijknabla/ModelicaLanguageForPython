3a7b3f568f4e8ee73097df5dcd4e90356d43dd9f
ijknable <ijknable@gmail.com>
2020-06-29 16:23:25 +0900

    Primitive types

--- modelica_language/typing/__init__.py
+++ modelica_language/typing/__init__.py
@@ -1,18 +1,28 @@
 
 __all__ = [
-    "Real",
-    "Integer",
-    "Boolean",
+    "SupportsReal",
+    "SupportsInteger",
+    "SupportsBoolean",
+    "PrimitiveReal",
+    "PrimitiveInteger",
+    "PrimitiveBoolean",
 ]
 
-from numbers import (
-    Real,
-    Integral as Integer,
-)
 
 from typing_extensions import Protocol
+import numpy  # type: ignore
+
+from numbers import (
+    Real as SupportsReal,
+    Integral as SupportsInteger,
+)
 
 
 class Boolean(Protocol):
     def __bool__(self) -> bool:
         ...
+
+
+PrimitiveReal = numpy.double
+PrimitiveInteger = numpy.intc
+PrimitiveBoolean = numpy.bool
