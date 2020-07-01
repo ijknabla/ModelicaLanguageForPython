ed515ada76c23f94f40838f0c494c238c418f109
ijknabla <ijknabla@gmail.com>
2020-07-01 12:21:58 +0900

    Add abstractmethod & runtime_checkable

--- modelica_language/typing.py
+++ modelica_language/typing.py
@@ -1,11 +1,18 @@
 
-from typing_extensions import Protocol
+from typing_extensions import (
+    Protocol,
+    runtime_checkable
+)
+import abc
 
 
+@runtime_checkable
 class SupportsModelicaLiteral(Protocol):
+    @abc.abstractmethod
     def __to_literal__(self) -> str:
         ...
 
     @classmethod
+    @abc.abstractmethod
     def __from_literal__(cls, literal: str) -> "SupportsModelicaLiteral":
         ...
