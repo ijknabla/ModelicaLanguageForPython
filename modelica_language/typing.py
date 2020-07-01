ee987a7610986ab35d0915596b143b86f69436ee
ijknabla <ijknabla@gmail.com>
2020-07-01 12:49:13 +0900

    Add primitive types protocol

--- modelica_language/typing.py
+++ modelica_language/typing.py
@@ -16,3 +16,33 @@ class SupportsLiteral(Protocol):
     @abc.abstractmethod
     def __from_literal__(cls, literal: str) -> "SupportsLiteral":
         ...
+
+
+@runtime_checkable
+class SupportsReal(Protocol, SupportsLiteral):
+    def __to_literal__(self) -> str:
+        return str(float(self))
+
+    @abc.abstractmethod
+    def __float__(self) -> float:
+        ...
+
+
+@runtime_checkable
+class SupportsInteger(Protocol, SupportsLiteral):
+    def __to_literal__(self) -> str:
+        return str(int(self))
+
+    @abc.abstractmethod
+    def __int__(self) -> int:
+        ...
+
+
+@runtime_checkable
+class SupportsBoolean(Protocol, SupportsLiteral):
+    def __to_literal__(self) -> str:
+        return "true" if self else "false"
+
+    @abc.abstractmethod
+    def __bool__(self) -> bool:
+        ...
