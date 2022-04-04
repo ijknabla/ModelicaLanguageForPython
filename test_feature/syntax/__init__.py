96dc944d627573e81bd599a47e4a074260c0afe6
ijknabla <ijknabla@gmail.com>
2022-04-04 15:05:32 +0900

    Fix format

--- test_feature/syntax/__init__.py
+++ test_feature/syntax/__init__.py
@@ -1,11 +1,7 @@
-
 import functools
-
 import pkg_resources
 
 
 @functools.lru_cache(1)
 def v3_4() -> str:
-    return pkg_resources.resource_string(
-        __name__, "v3-4.peg",
-    ).decode("ASCII")
+    return pkg_resources.resource_string(__name__, "v3-4.peg").decode("ASCII")
