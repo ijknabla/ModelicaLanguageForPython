fbbbc37ab916ebdf296acfbcd4dbf136eead6655
ijknabla <ijknabla@gmail.com>
2022-04-05 11:43:51 +0900

    Add test for syntax

--- test_feature/test_1st.py
+++ test_feature/test_1st.py
@@ -1,5 +1,26 @@
+from arpeggio import ParserPython
+import pytest
+
+from .parser import comment, grammar
 from .syntax import v3_4
 
 
 def test_syntax() -> None:
     assert isinstance(v3_4(), str)
+
+
+dialects = [
+    "",
+    """
+IDENT |= r"\\$\\w*"
+    """,
+    """
+file: stored-definition @EOF@
+    """,
+]
+
+
+@pytest.mark.parametrize("dialect", dialects)
+def test_parser(dialect: str) -> None:
+    parser = ParserPython(grammar, comment)
+    parser.parse(v3_4() + dialect)
