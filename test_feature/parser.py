99f0581b621b82fabdde4330755002eea19b6e0c
ijknabla <ijknabla@gmail.com>
2022-04-05 11:53:52 +0900

    Implement keywords_assignment

--- test_feature/parser.py
+++ test_feature/parser.py
@@ -1,10 +1,30 @@
-from arpeggio import EOF, RegExMatch
+from arpeggio import EOF, OneOrMore, RegExMatch
 from typing import Any
+
+
+# lexical rules
+def KEYWORD() -> RegExMatch:
+    return RegExMatch("`[a-z]+`")
 
 
 # grammar rules
 def grammar() -> Any:
-    return (EOF,)
+    return (
+        OneOrMore(lexical_rule),
+        EOF,
+    )
+
+
+def lexical_rule() -> Any:
+    return [keywords_assignment]
+
+
+def keywords_assignment() -> Any:
+    return (
+        "@KEYWORDS@",
+        ["=", "|="],
+        OneOrMore(KEYWORD, sep="|"),
+    )
 
 
 # comment rule
