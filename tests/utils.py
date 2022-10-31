0454467c6e2e10d58693df1eac30aa38f8009334
ijknabla <ijknabla@gmail.com>
2022-10-31 11:11:22 +0900

    Implement flatten function

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,7 @@
 from functools import wraps
-from typing import Callable, Dict, TypeVar
+from typing import Callable, Dict, Optional, TypeVar
+
+import arpeggio
 
 T_1 = TypeVar("T_1")
 T_2 = TypeVar("T_2")
@@ -16,3 +18,24 @@ def assert_injective(f: Callable[[T_1], T_2]) -> Callable[[T_1], T_2]:
         return y
 
     return wrapped
+
+
+def flatten(
+    parent: arpeggio.ParsingExpression,
+    result: Optional[Dict[str, arpeggio.ParsingExpression]] = None,
+) -> Dict[str, arpeggio.ParsingExpression]:
+    if result is None:
+        result = {}
+
+    if parent.rule_name not in result:
+
+        if parent.rule_name:
+            result[parent.rule_name] = parent
+
+        if isinstance(parent, arpeggio.Repetition) and parent.sep is not None:
+            flatten(parent.sep, result)
+
+        for child in parent.nodes:
+            flatten(child, result)
+
+    return result
