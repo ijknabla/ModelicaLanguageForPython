b1b56a8800da846923738f4e5314fe04ecd9aa1f
ijknabla <ijknabla@gmail.com>
2023-01-11 23:45:44 +0900

    tmp

--- future/_ast_generator.py
+++ future/_ast_generator.py
@@ -1,3 +1,4 @@
+import re
 import typing
 from ast import (
     AnnAssign,
@@ -20,6 +21,40 @@ from ast import (
     expr,
     expr_context,
     stmt,
+)
+from dataclasses import dataclass, field
+from functools import reduce
+from operator import or_
+from typing import (
+    Collection,
+    Iterator,
+    List,
+    NewType,
+    Optional,
+    Protocol,
+    Sequence,
+    Set,
+    Union,
+)
+
+from arpeggio import (
+    EndOfFile,
+    NoMatch,
+    OneOrMore,
+    ParseTreeNode,
+    PTNodeVisitor,
+    RegExMatch,
+    Terminal,
+    visit_parse_tree,
+)
+
+from modelica_language import ParserPython
+
+from ._types import (
+    ParsingExpressionLike,
+    Regex,
+    Text,
+    returns_parsing_expression,
 )
 
 
@@ -137,3 +172,247 @@ def create_tuple(
     if ctx is None:
         ctx = Load()
     return Tuple(elts=elts, ctx=ctx)
+
+
+CharacterCode = NewType("CharacterCode", int)
+CharacterCodeSet = Set[CharacterCode]
+
+
+class ResolvablePattern:
+    def resolve(self) -> "Pattern":
+        raise NotImplementedError()
+
+
+@dataclass
+class PatternReference(ResolvablePattern):
+    target: Optional["Pattern"] = field(default=None)
+
+    def resolve(self) -> "Pattern":
+        assert self.target is not None
+        return resolve_pattern(self.target)
+
+
+@dataclass(frozen=True)
+class RepetitionPatternBase(ResolvablePattern):
+    pattern: "Pattern"
+
+
+class OptionalPattern(RepetitionPatternBase):
+    def resolve(self) -> "Pattern":
+        return self.__class__(resolve_pattern(self.pattern))
+
+
+class ZeroOrMorePattern(RepetitionPatternBase):
+    def resolve(self) -> "Pattern":
+        return self.__class__(resolve_pattern(self.pattern))
+
+
+@dataclass(frozen=True)
+class SequencePatternBase(ResolvablePattern):
+    patterns: Sequence["Pattern"]
+
+    def __iter__(self) -> Iterator["Pattern"]:
+        yield from self.patterns
+
+    @classmethod
+    def _flatten(cls, *patterns: "Pattern") -> Iterator["Pattern"]:
+        if not patterns:
+            return
+
+        head, *tail = patterns
+        head = resolve_pattern(head)
+        if isinstance(head, cls):
+            yield from head
+        else:
+            yield head
+
+        yield from cls._flatten(*tail)
+
+
+class SequencePattern(SequencePatternBase):
+    def resolve(self) -> "Pattern":
+        head, *tail = self._flatten(*self)
+        if tail:
+            return SequencePattern((head, *tail))
+        else:
+            return head
+
+
+class OrderedChoicePattern(SequencePatternBase):
+    def resolve(self) -> "Pattern":
+        character_code_set: CharacterCodeSet = set()
+        patterns: List[Pattern] = []
+
+        for pattern in self._flatten(*self):
+            if isinstance(pattern, set):
+                character_code_set.update(pattern)
+            else:
+                patterns.append(pattern)
+
+        if character_code_set:
+            patterns.insert(0, character_code_set)
+
+        if len(patterns) == 1:
+            return patterns[0]
+        else:
+            return OrderedChoicePattern(tuple(patterns))
+
+
+Pattern = Union[
+    CharacterCodeSet,
+    Regex,
+    PatternReference,
+    OptionalPattern,
+    ZeroOrMorePattern,
+    SequencePattern,
+    OrderedChoicePattern,
+]
+
+NotOrderedChoicePattern = Union[
+    CharacterCodeSet,
+    Regex,
+    PatternReference,
+    OptionalPattern,
+    ZeroOrMorePattern,
+    SequencePattern,
+    # OrderedChoicePattern,
+]
+
+
+def lexical_text(text: Text) -> Union[CharacterCodeSet, Regex]:
+    if len(text) == 1:
+        return {CharacterCode(ord(text))}
+    else:
+        return Regex(re.escape(text))
+
+
+def lexical_regex(regex: Regex) -> Union[CharacterCodeSet, Regex]:
+    try:
+        character_code_set: CharacterCodeSet = visit_parse_tree(
+            character_set_parser.parse(regex), CharacterSetVisitor()
+        )
+        return character_code_set
+    except NoMatch:
+        return Regex(regex)
+
+
+def to_regex(pattern: Pattern) -> str:
+    check_recursion(pattern)
+    pattern = resolve_pattern(pattern)
+    return _to_regex(pattern, root=True)
+
+
+def check_recursion(pattern: Pattern) -> None:
+    ...
+
+
+def resolve_pattern(pattern: Pattern) -> Pattern:
+    if isinstance(pattern, ResolvablePattern):
+        return pattern.resolve()
+    else:
+        return pattern
+
+
+def _to_regex(pattern: Pattern, root: bool = False) -> str:
+    if isinstance(pattern, SequencePattern):
+        content = "".join(map(_to_regex, pattern))
+        if not root:
+            return "(" + content + ")"
+        else:
+            return content
+    elif isinstance(pattern, OrderedChoicePattern):
+        content = "|".join(map(_to_regex, pattern))
+        if not root:
+            return "(" + content + ")"
+        else:
+            return content
+    elif isinstance(pattern, set):
+        if len(pattern) == 1:
+            (char,) = map(chr, pattern)
+            return re.escape(char)
+        else:
+            groups = []
+            for category in categorize(pattern):
+                groups.append(
+                    "-".join(
+                        map(
+                            re.escape,
+                            map(
+                                chr,
+                                [category[0]]
+                                if len(category) == 1
+                                else [category[0], category[-1]],
+                            ),
+                        )
+                    )
+                )
+            return "[" + "".join(groups) + "]"
+    elif isinstance(pattern, str):
+        return pattern
+    elif isinstance(pattern, OptionalPattern):
+        return _to_regex(pattern.pattern) + "?"
+    elif isinstance(pattern, ZeroOrMorePattern):
+        return _to_regex(pattern.pattern) + "*"
+    raise NotImplementedError()
+
+
+@returns_parsing_expression
+def grammar() -> ParsingExpressionLike:
+    return "[", OneOrMore(character_set), "]", EndOfFile()
+
+
+@returns_parsing_expression
+def character_set() -> ParsingExpressionLike:
+    return [character_range, character]
+
+
+@returns_parsing_expression
+def character() -> ParsingExpressionLike:
+    return RegExMatch(r"\\.|[^\^\[\]\\]")
+
+
+@returns_parsing_expression
+def character_range() -> ParsingExpressionLike:
+    return character, "-", character
+
+
+character_set_parser = ParserPython(grammar, skipws=False)
+
+
+class SupportsChildren(Protocol):
+    character: Sequence[CharacterCodeSet]
+    character_set: Sequence[CharacterCodeSet]
+
+
+class CharacterSetVisitor(PTNodeVisitor):
+    def visit_character(
+        self, node: Terminal, _: SupportsChildren
+    ) -> CharacterCodeSet:
+        char = re.sub(r"^\\", "", node.value)
+        return {CharacterCode(ord(char))}
+
+    def visit_character_range(
+        self, _: ParseTreeNode, children: SupportsChildren
+    ) -> CharacterCodeSet:
+        (begin,), (end,) = children.character
+        return set(map(CharacterCode, range(begin, end + 1)))
+
+    def visit_grammar(
+        self, _: ParseTreeNode, children: SupportsChildren
+    ) -> CharacterCodeSet:
+        return reduce(or_, children.character_set)
+
+
+def categorize(items: Collection[int]) -> Iterator[List[int]]:
+    items = sorted(items)
+    partition = [
+        i
+        for i, (j, k) in enumerate(zip(items[:-1], items[1:]), start=1)
+        if j + 1 < k
+    ]
+
+    for start, stop in zip(
+        [0, *partition],
+        [*partition, None],
+    ):
+        yield items[start:stop]
--- future/_peg_visitor.py
+++ future/_peg_visitor.py
@@ -1,9 +1,23 @@
 from ast import AnnAssign, Constant, Ellipsis, FunctionDef, Module
-from typing import Any, Iterable, Iterator, Protocol, Set
+from typing import (
+    Any,
+    DefaultDict,
+    Iterable,
+    Iterator,
+    Protocol,
+    Sequence,
+    Set,
+)
 
-from arpeggio import NonTerminal, PTNodeVisitor, Terminal
+from arpeggio import NonTerminal, ParseTreeNode, PTNodeVisitor, Terminal
 
 from ._ast_generator import (
+    OptionalPattern,
+    OrderedChoicePattern,
+    Pattern,
+    PatternReference,
+    SequencePattern,
+    ZeroOrMorePattern,
     create_ann_assign,
     create_attribute,
     create_call,
@@ -11,22 +25,37 @@ from ._ast_generator import (
     create_module_with_class,
     create_subscript,
     create_tuple,
+    lexical_regex,
+    lexical_text,
 )
 from ._types import Keyword, Regex, Rule, Text
 
 
 class SupportsChildren(Protocol):
-    ...
+    KEYWORD: Sequence[Keyword]
+    LEXICAL_REFERENCE: Sequence[Rule]
+    REGEX: Sequence[Regex]
+    TEXT: Sequence[Text]
+    lexical_expression: Sequence[Pattern]
+    lexical_ordered_choice: Sequence[Pattern]
+    lexical_primary: Sequence[Pattern]
+    lexical_quantity: Sequence[Pattern]
+    lexical_sequence: Sequence[Pattern]
+
+
+PatternReferences = DefaultDict[Rule, PatternReference]
 
 
 class ModuleVisitor(PTNodeVisitor):
     class_name: str
     keywords: Set[Keyword]
+    pattern_references: PatternReferences
 
     def __init__(self, class_name: str, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.class_name = class_name
         self.keywords = set()
+        self.pattern_references = PatternReferences(PatternReference)
 
     def visit_KEYWORD(self, node: Terminal, _: SupportsChildren) -> Keyword:
         keyword = Keyword(node.value[1:-1])
@@ -48,6 +77,56 @@ class ModuleVisitor(PTNodeVisitor):
 
     visit_LEXICAL_RULE = __visit_RULE
     visit_SYNTAX_RULE = __visit_RULE
+
+    def visit_lexical_primary(
+        self, _: ParseTreeNode, children: SupportsChildren
+    ) -> Pattern:
+        if children.lexical_expression:
+            (expression,) = children.lexical_expression
+            return expression
+        elif children.REGEX:
+            (regex,) = children.REGEX
+            return lexical_regex(regex)
+        elif children.TEXT:
+            (text,) = children.TEXT
+            return lexical_text(text)
+        elif children.LEXICAL_REFERENCE:
+            (rule,) = children.LEXICAL_REFERENCE
+            return self.pattern_references[rule]
+        raise NotImplementedError()
+
+    def visit_lexical_quantity(
+        self, node: NonTerminal, children: SupportsChildren
+    ) -> Pattern:
+        if children.lexical_expression:
+            (child,) = children.lexical_expression
+            L, _, R = node
+            assert isinstance(L, Terminal)
+            assert isinstance(R, Terminal)
+            if L == "[" and R == "]":
+                return OptionalPattern(child)
+            elif L == "{" and R == "}":
+                return ZeroOrMorePattern(child)
+        elif children.lexical_primary:
+            (child,) = children.lexical_primary
+            return child
+        raise NotImplementedError()
+
+    def visit_lexical_sequence(
+        self, _: ParseTreeNode, children: SupportsChildren
+    ) -> Pattern:
+        return SequencePattern(children.lexical_quantity)
+
+    def visit_lexical_ordered_choice(
+        self, _: ParseTreeNode, children: SupportsChildren
+    ) -> Pattern:
+        return OrderedChoicePattern(children.lexical_sequence)
+
+    def visit_lexical_expression(
+        self, _: ParseTreeNode, children: SupportsChildren
+    ) -> Pattern:
+        (child,) = children.lexical_ordered_choice
+        return child
 
     def visit_grammar(self, _1: PTNodeVisitor, _2: SupportsChildren) -> Module:
         sorted_keywords = sorted(self.keywords)
