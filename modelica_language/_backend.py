8d99df70ef6886499bb1d5984af609a1889e3dff
ijknabla <ijknabla@gmail.com>
2023-01-12 15:20:15 +0900

    Implement decorator for arpeggio rule

--- modelica_language/_backend.py
+++ modelica_language/_backend.py
@@ -1,10 +1,21 @@
 __all__ = (
     "ParsingExpression",
     "ParsingExpressionLike",
+    "returns_parsing_expression",
 )
 
+from typing import Callable, cast
+
 import arpeggio
-from typing_extensions import TypeAlias
+from typing_extensions import ParamSpec, TypeAlias
+
+P = ParamSpec("P")
 
 ParsingExpression = arpeggio.ParsingExpression
 ParsingExpressionLike: TypeAlias = "arpeggio._ParsingExpressionLike"
+
+
+def returns_parsing_expression(
+    f: Callable[P, ParsingExpressionLike]
+) -> Callable[P, ParsingExpression]:
+    return cast(Callable[P, ParsingExpression], f)
