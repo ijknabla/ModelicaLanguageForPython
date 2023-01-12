a8444d5e1ba42b40dd45011e9c6c67624ffc7770
ijknabla <ijknabla@gmail.com>
2023-01-12 17:41:27 +0900

    Deprecate test for ParserPEG

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,75 +1,42 @@
-import enum
-from typing import Any
-
 import pytest
-from arpeggio import EndOfFile, Parser
+from arpeggio import EndOfFile, RegExMatch
 
-from modelica_language import ParserPEG, ParserPython
-from modelica_language.syntax import v3_4_peg
+from modelica_language import ParserPython
+from modelica_language._backend import (
+    ParsingExpressionLike,
+    returns_parsing_expression,
+)
 from modelica_language.syntax.v3_4 import Syntax
 
 
-@pytest.fixture(scope="module")
-def ident_parser() -> ParserPEG:
-    return ParserPEG(
-        f"""
-{v3_4_peg()}
-file: IDENT $EOF$
-        """,
-        "file",
-    )
+@returns_parsing_expression
+def file() -> ParsingExpressionLike:
+    return Syntax.stored_definition, EndOfFile()
+
+
+@returns_parsing_expression
+def ident() -> ParsingExpressionLike:
+    return Syntax.IDENT, EndOfFile()
+
+
+@returns_parsing_expression
+def ident_dialect() -> ParsingExpressionLike:
+    return [Syntax.IDENT, RegExMatch(r"\$\w+")], EndOfFile()
 
 
 @pytest.fixture(scope="module")
-def ident_dialect_parser() -> ParserPEG:
-    return ParserPEG(
-        f"""
-{v3_4_peg()}
-IDENT |= r'\\$\\w*'
-file: IDENT $EOF$
-        """,
-        "file",
-    )
-
-
-@pytest.fixture(scope="module")
-def peg_file_parser() -> ParserPEG:
-    return ParserPEG(
-        f"""
-{v3_4_peg()}
-file: stored-definition $EOF$
-        """,
-        "file",
-        "COMMENT",
-    )
-
-
-@pytest.fixture(scope="module")
-def py_file_parser() -> ParserPython:
-    def file() -> Any:
-        return Syntax.stored_definition, EndOfFile()
-
+def file_parser() -> ParserPython:
     return ParserPython(
         file,
         Syntax.CPP_STYLE_COMMENT,
     )
 
 
-class ParserParmeter(enum.Enum):
-    py = enum.auto()
-    peg = enum.auto()
+@pytest.fixture(scope="module")
+def ident_parser() -> ParserPython:
+    return ParserPython(ident, Syntax.CPP_STYLE_COMMENT)
 
 
-@pytest.fixture(scope="module", params=ParserParmeter)
-def file_parser(
-    request: pytest.FixtureRequest,
-    peg_file_parser: ParserPEG,
-    py_file_parser: ParserPython,
-) -> Parser:
-    param = request.param  # type: ignore
-    if param is ParserParmeter.peg:
-        return peg_file_parser
-    elif param is ParserParmeter.py:
-        return py_file_parser
-    else:
-        raise NotImplementedError()
+@pytest.fixture(scope="module")
+def ident_dialect_parser() -> ParserPython:
+    return ParserPython(ident_dialect, Syntax.CPP_STYLE_COMMENT)
