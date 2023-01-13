2f42f3a27bb4eda3a20dc0cca590e66422d9d3bd
ijknabla <ijknabla@gmail.com>
2023-01-14 04:07:36 +0900

    Implement get_file_parser, get_syntax_type

--- modelica_language/__init__.py
+++ modelica_language/__init__.py
@@ -1,9 +1,16 @@
 __all__ = (
     "ParsingExpressionLike",
     "enable_method_in_parser_python",
+    "get_file_parser",
+    "get_syntax_type",
     "returns_parsing_expression",
     "v3_4",
 )
+
+import enum
+from typing import Optional, Type
+
+from arpeggio import EOF, ParserPython
 
 from . import v3_4
 from ._backend import (
@@ -11,3 +18,32 @@ from ._backend import (
     enable_method_in_parser_python,
     returns_parsing_expression,
 )
+
+latest = v3_4
+
+
+class ModelicaVersion(enum.Enum):
+    latest = v3_4 = enum.auto()
+
+
+_SYNTAXES = {ModelicaVersion.v3_4: v3_4.Syntax}
+
+
+@enable_method_in_parser_python
+def get_file_parser(version: Optional[ModelicaVersion] = None) -> ParserPython:
+    syntax_type = get_syntax_type(version)
+
+    @returns_parsing_expression
+    def file() -> ParsingExpressionLike:
+        return syntax_type.stored_definition, EOF
+
+    return ParserPython(file, syntax_type.COMMENT)
+
+
+def get_syntax_type(
+    version: Optional[ModelicaVersion] = None,
+) -> Type[v3_4.Syntax]:
+    return _SYNTAXES.get(
+        version,  # type: ignore
+        latest.Syntax,
+    )
