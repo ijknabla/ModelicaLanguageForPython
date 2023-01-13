cdad0fcd8a5053b9c8f5b88f1093d1170e121b1c
ijknabla <ijknabla@gmail.com>
2023-01-14 01:31:17 +0900

    Add backend contents to package

--- modelica_language/__init__.py
+++ modelica_language/__init__.py
@@ -1,7 +1,15 @@
 __all__ = (
     "ParserPython",
+    "ParsingExpressionLike",
+    "enable_method_in_parser_python",
+    "returns_parsing_expression",
     "v3_4",
 )
 
 from . import v3_4
+from ._backend import (
+    ParsingExpressionLike,
+    enable_method_in_parser_python,
+    returns_parsing_expression,
+)
 from ._parser import ParserPython
