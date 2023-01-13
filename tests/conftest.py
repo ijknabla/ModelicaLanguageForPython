33c1210977df2020e11ae48b63c4b4e59a51652e
ijknabla <ijknabla@gmail.com>
2023-01-14 03:24:34 +0900

    Stop use EOF

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from arpeggio import EndOfFile, ParserPython, RegExMatch
+from arpeggio import EOF, ParserPython, RegExMatch
 
 from modelica_language._backend import (
     ParsingExpressionLike,
@@ -11,17 +11,17 @@ from modelica_language.v3_4 import Syntax
 
 @returns_parsing_expression
 def file() -> ParsingExpressionLike:
-    return Syntax.stored_definition, EndOfFile()
+    return Syntax.stored_definition, EOF
 
 
 @returns_parsing_expression
 def ident() -> ParsingExpressionLike:
-    return Syntax.IDENT, EndOfFile()
+    return Syntax.IDENT, EOF
 
 
 @returns_parsing_expression
 def ident_dialect() -> ParsingExpressionLike:
-    return [Syntax.IDENT, RegExMatch(r"\$\w+")], EndOfFile()
+    return [Syntax.IDENT, RegExMatch(r"\$\w+")], EOF
 
 
 @pytest.fixture(scope="module")
