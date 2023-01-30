2141caf9ffaf03c1c5fa763eb6c88277c9a09913
ijknabla <ijknabla@gmail.com>
2023-01-30 14:09:23 +0900

    Add sphinx-rtd-theme

--- document/conf.py
+++ document/conf.py
@@ -19,7 +19,10 @@ author = "ijknabla <ijknabla@gmail.com>"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []  # type: ignore
@@ -28,5 +31,5 @@ exclude_patterns = []  # type: ignore
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
